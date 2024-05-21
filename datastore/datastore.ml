54bb53c89a61741fb97433cd56c91594e9f0d32f
lubegasimon <lubegasimon73@gmail.com>
2024-05-21 08:27:48 +0300

    add: datastore to act as our db for now

---
+++ datastore/datastore.ml
@@ -0,0 +1,11 @@
+module Datastore = struct
+  let hashtbl = Hashtbl.create 10
+  let create_user (name : string) (id : string) = Hashtbl.add hashtbl name id
+  let find_user (name : string) = Hashtbl.find_opt hashtbl name
+  let remove_user (name : string) = Hashtbl.remove hashtbl name
+
+  let update_user (name : string) (id : string) =
+    Hashtbl.replace hashtbl name id
+
+  let length = Hashtbl.length hashtbl
+end
