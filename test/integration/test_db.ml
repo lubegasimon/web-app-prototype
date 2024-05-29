894692cce81e180681eabffbce484585d28df633
lubegasimon <lubegasimon73@gmail.com>
2024-05-29 11:50:20 +0300

    improve error handling using alcotest testables

--- test/integration/test_db.ml
+++ test/integration/test_db.ml
@@ -12,25 +12,25 @@ module To_test = struct
     Db.with_connection (fun conn -> Model.User.find_user conn email) db_uri
 end
 
-(* The tests *)
 let test_if_user_created _ () =
-  let expected = () in
-  let actual =
-    To_test.create_user "johndoe" "johndoe@gmail.com" "johndoe" >>= function
-    | Ok _ -> (
-        (* Ensure that the user is created *)
-        To_test.get_user_by_email "johndoe@gmail.com"
-        >>= function
-        | Ok (Some user_name) ->
-            Lwt_io.printf "User '%s' successfully created\n" user_name
-        | Ok None -> Lwt_io.printf "User not found in the database!\n"
-        | Error err -> Lwt_io.printf "%s\n" (Caqti_error.show err))
-    | Error err ->
-        Lwt_io.printf "Error fetching user: %s\n" (Caqti_error.show err)
-  in
-  actual >|= Alcotest.(check unit) "user" expected
+  let expected_name = "johndoe" in
+  let email = "johndoe@gmail.com" in
+  let password = "johndoe" in
+  To_test.create_user expected_name email password >>= function
+  | Ok _ -> (
+      (* Ensure that the user is created *)
+      To_test.get_user_by_email email
+      >>= function
+      | Ok (Some name) ->
+          let actual_name = name in
+          Alcotest.(check string) "same name" expected_name actual_name;
+          Lwt.return_unit
+      | Ok None -> Alcotest.fail "User not found in the database!\n"
+      | Error err ->
+          Lwt_io.printf "Error fetching user: %s\n" (Caqti_error.show err))
+  | Error err ->
+      Alcotest.failf "Error creating user: %s\n" (Caqti_error.show err)
 
-(* Run it *)
 let () =
   let open Alcotest_lwt in
   Lwt_main.run
