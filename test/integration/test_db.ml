8b0881b3da75c273c33ea65486a37179d92ea411
lubegasimon <lubegasimon73@gmail.com>
2024-05-29 09:28:43 +0300

    add 'find_user_by_email' to ensure that 'create_user' query is a success

--- test/integration/test_db.ml
+++ test/integration/test_db.ml
@@ -1,10 +1,15 @@
 open Lwt.Infix
 
 module To_test = struct
+  let db_uri = "TEST_DATABASE_URI"
+
   let create_user name email password =
     Db.with_connection
       (fun conn -> Model.User.create_user conn name email password)
-      "TEST_DATABASE_URI"
+      db_uri
+
+  let get_user_by_email email =
+    Db.with_connection (fun conn -> Model.User.find_user conn email) db_uri
 end
 
 (* The tests *)
@@ -12,9 +17,16 @@ let test_if_user_created _ () =
   let expected = () in
   let actual =
     To_test.create_user "johndoe" "johndoe@gmail.com" "johndoe" >>= function
-    | Ok _ -> Lwt.return_unit
+    | Ok _ -> (
+        (* Ensure that the user is created *)
+        To_test.get_user_by_email "johndoe@gmail.com"
+        >>= function
+        | Ok (Some user_name) ->
+            Lwt_io.printf "User '%s' successfully created\n" user_name
+        | Ok None -> Lwt_io.printf "User not found in the database!\n"
+        | Error err -> Lwt_io.printf "%s\n" (Caqti_error.show err))
     | Error err ->
-        Lwt_io.printf "Error creating user: %s\n" (Caqti_error.show err)
+        Lwt_io.printf "Error fetching user: %s\n" (Caqti_error.show err)
   in
   actual >|= Alcotest.(check unit) "user" expected
 
