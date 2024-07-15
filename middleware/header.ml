e5fbff358537f02380a659c4648f952eb9fe2762
lubegasimon <lubegasimon73@gmail.com>
2024-07-15 16:19:17 +0300

    add X-Csrf-token to headers

--- middleware/header.ml
+++ middleware/header.ml
@@ -4,6 +4,6 @@ let csrf_token = Uuidm.v4 (Bytes.create 16) |> Uuidm.to_string
 let headers =
   Cohttp.Header.of_list
     [
-      ("Set-Cookie", Format.sprintf "session_id=%s" session_id);
-      ("Set-Cookie", Format.sprintf "csrf-token=%s" csrf_token);
+      ("Set-Cookie", Format.sprintf "session_id=%s; HttpOnly; secure" session_id);
+      ("X-Csrf-token", Format.sprintf "%s" csrf_token);
     ]
