86e6f1d54b7ae0f19fcf713ee2b9c9ee7f8f1007
lubegasimon <lubegasimon73@gmail.com>
2024-07-15 16:16:00 +0300

    integrate logout handler with redis

--- handler/logout.ml
+++ handler/logout.ml
@@ -1,19 +1,19 @@
 open Lwt
 open Cohttp_lwt_unix
+open Middleware
 
-let logout req =
+let logout req body =
+  (* TODO: body seems redundant *)
+  let _ = body in
   let cookies = Cohttp.Cookie.Cookie_hdr.extract (Request.headers req) in
   let session_id = List.assoc_opt "session_id" cookies in
   match session_id with
-  | Some id -> (
-      Db.with_connection
-        (fun conn -> Model.User_session.close_session conn id)
-        "DATABASE_URI"
-      >>= fun res ->
-      match res with
+  | Some session_id -> (
+      Redis.redis_conn >>= fun conn ->
+      Redis.delete_session ~conn ~session_id >>= function
       | Ok _ -> Server.respond_redirect ~uri:(Uri.of_string "/signup") ()
       | Error err ->
           Server.respond_error ~status:`Internal_server_error
-            ~body:(Middleware.Error.to_string (Database_error err))
+            ~body:(Error.to_string (Database_error err))
             ())
   | None -> Server.respond_redirect ~uri:(Uri.of_string "/signup") ()
